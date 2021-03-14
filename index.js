dfa6fa349f27d821e62b49a1a869157a914b31e4
jim <jbmbroomfield@gmail.com>
2021-03-14 17:25:16 +1100

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet (someValue) {
+    return 264 * distanceFromHqInBlocks(someValue);
+}
+
+// block is 264 ft
+function distanceTravelledInFeet(a,b) {
+    // return Math.abs(distanceFromHqInFeet(a)-distanceFromHqInFeet(b));
+    return 264 * Math.abs(a-b);
+}
+
+function calculatesFarePrice (a,b) {
+    const dist = distanceTravelledInFeet(a,b);
+    if (dist <= 400) {
+        return 0;
+    } else if (dist <= 2000) {
+        return (dist-400) * 0.02;
+    } else if (dist < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
